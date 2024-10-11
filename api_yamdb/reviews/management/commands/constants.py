e0bc82269d443b15b1523cc668b2088575287fd1
Artemii Tretiakov <artemii.tretiakov@gmail.com>
2024-10-11 11:40:47 +0300

    import csv improved

--- api_yamdb/reviews/management/commands/constants.py
+++ api_yamdb/reviews/management/commands/constants.py
@@ -1,10 +1,22 @@
-MODELS_TO_LOAD = ['category',
-                  'genre',
-                  'title',
-                  'title_genre',
-                  'users',
-                  'review',
-                  'comment', ]
+MTM_MODELS = ['title_genre',]
 
-MTM_MODELS = ['title_genre',
-              ]
+APPS = [
+    'users',
+    'reviews',
+        ]
+
+PATH = 'static/data'
+
+APPS_AND_MODELS = {
+    'reviews': [
+        'category',
+        'genre',
+        'title',
+        'title_genre',
+        'review',
+        'comment', 
+    ],
+    'users': [
+        'user',
+        ]
+}
