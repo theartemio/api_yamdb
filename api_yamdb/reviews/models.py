0756bef30c916610b08c23ad83d08ec4d980254f
Artemii Tretiakov <artemii.tretiakov@gmail.com>
2024-10-13 19:26:06 +0300

    .

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,14 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from .constants import MAX_SLUG_L, MAX_NAME_L
 
 User = get_user_model()
 
 
 class NameSlugMixin(models.Model):
     """Миксин для простых моделей с двумя полями."""
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
+
+    name = models.CharField(max_length=MAX_NAME_L, verbose_name="Название")
+    slug = models.SlugField(
+        unique=True,
+        max_length=MAX_SLUG_L,
+        verbose_name="Слаг",
+        help_text=(
+            "Уникальный слаг, по которому можно указать жанр",
+            "или категорию. Рекомендуется использовать понятный",
+            "слаг, например транслитерацию или английское название.",
+        ),
+    )
 
     class Meta:
         abstract = True
@@ -19,11 +30,13 @@ class NameSlugMixin(models.Model):
 
 class Category(NameSlugMixin, models.Model):
     """Модель для хранения категорий."""
+
     pass
 
 
 class Genre(NameSlugMixin, models.Model):
     """Модель для хранения жанров."""
+
     pass
 
 
@@ -36,56 +49,83 @@ class Title(models.Model):
         у одного произведения допускается несколько жанров,
         переданных списком.
     """
-    name = models.CharField(max_length=256)
-    year = models.PositiveSmallIntegerField()
-    description = models.TextField(blank=True, null=True)
-    genre = models.ManyToManyField(Genre,
-                                   related_name='genres')
-    category = models.ForeignKey(Category,
-                                 on_delete=models.SET_NULL,
-                                 related_name="categories",
-                                 null=True,
-                                 blank=False
-                                 )
-    rating = models.PositiveSmallIntegerField(null=True)
+
+    name = models.CharField(
+        max_length=MAX_NAME_L,
+        verbose_name="Название",
+        help_text="Официальное название произведения.",
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name="Год", help_text="Год выпуска произведения."
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание",
+        help_text="Краткое описание произведения.",
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        related_name="genres",
+        verbose_name="Жанры",
+        help_text=("Жанры, к которым относится произведение.",
+                   " Может быть несколько."),
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="categories",
+        null=True,
+        blank=False,
+        verbose_name="Категория",
+        help_text="Категория, например 'фильм' или 'музыка'.",
+    )
+    rating = models.PositiveSmallIntegerField(
+        null=True,
+        verbose_name="Рейтинг",
+        help_text=("Средний рейтинг. Считается по",
+                   " всем опубликованным рецензиям."),
+    )
 
     def recalculate_rating(self):
         reviews = self.reviews.all()
         if reviews.exists():
-            self.rating = (sum([review.score for review in reviews])
-                           / reviews.count())
+            self.rating = (
+                sum([review.score for review in reviews]) / reviews.count()
+            )
         else:
             self.rating = None
         self.save()
 
     def __str__(self):
-        return f'{self.name} {self.genre} {self.category}'
+        return f"{self.name} {self.genre} {self.category}"
 
 
 class Review(models.Model):
     """
     Модель для представления отзыва на произведение.
     """
+
     title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        null=True
+        Title, on_delete=models.CASCADE, related_name="reviews", null=True
     )
     text = models.TextField()
     score = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(10)]
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="reviews")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews"
+    )
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
         old_score = None
         if self.pk:
-            old_score = (Review.objects.
-                         filter(pk=self.pk).
-                         values_list('score', flat=True).first())
+            old_score = (
+                Review.objects.filter(pk=self.pk)
+                .values_list("score", flat=True)
+                .first()
+            )
         super().save(*args, **kwargs)
         if old_score != self.score:
             self.title.recalculate_rating()
@@ -97,12 +137,12 @@ class Review(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['title', 'author'],
-                name='unique_review',
+                fields=["title", "author"],
+                name="unique_review",
             )
         ]
 
-        ordering = ['-pub_date']
+        ordering = ["-pub_date"]
 
     def __str__(self):
         return self.text[:50]
@@ -112,18 +152,20 @@ class Comment(models.Model):
     """
     Модель для представления комментария.
     """
+
     review = models.ForeignKey(
         Review,
         on_delete=models.CASCADE,
-        related_name='comments',
+        related_name="comments",
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['pub_date']
+        ordering = ["pub_date"]
 
     def __str__(self):
         return self.text[:50]
